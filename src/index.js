b5b11e79415f5d8b5ffa1033498d6411c23d2975
Hydrogen-Deuteride <74467714+Hydrogen-Deuteride@users.noreply.github.com>
2020-11-14 23:35:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import Waifu2x from './Waifu2x';
-
-(function(global) {
-    if(typeof(window) !== 'undefined') {
-        window.Waifu2x = Waifu2x;
-        process && (process.versions = {});
-    }
-    if ('process' in global) {
-        return;
-    }
-    global.Waifu2x = Waifu2x;
-})((this || 0).self || global);
-
-export default Waifu2x;
+let Waifu2x = require('./Waifu2x');
+module.exports = Waifu2x;
